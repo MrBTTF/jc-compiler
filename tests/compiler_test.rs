64b5390ec191ee6f2e228698de96e34fc6a00094
MrBTTF <zhvladi@gmail.com>
2024-05-31 17:33:38 +0200

    global tests

---
+++ tests/compiler_test.rs
@@ -0,0 +1,34 @@
+use std::env;
+use std::process::Command;
+
+fn compile_src(src: &str) -> String {
+    let dest = env::current_dir()
+        .unwrap()
+        .join(&format!("local/bin/{src}.exe"));
+    let src = env::current_dir()
+        .unwrap()
+        .join(&format!("tests/fixtures/{src}.jc"));
+    Command::new("cargo")
+        .args(&["run", src.to_str().unwrap(), dest.to_str().unwrap()])
+        .output()
+        .unwrap();
+    let stdout = Command::new(dest.to_str().unwrap())
+        .output()
+        .unwrap()
+        .stdout;
+    String::from_utf8(stdout).unwrap()
+}
+
+#[test]
+fn test_hello() {
+    let src = "hello";
+    let output = compile_src(&src);
+    assert_eq!(&output, "Hello World!\r\nNummer33\r\nSome test\r\n199Qwerty\r\n")
+}
+
+#[test]
+fn test_loop() {
+    let src = "loop";
+    let output = compile_src(&src);
+    assert_eq!(&output, "")
+}
