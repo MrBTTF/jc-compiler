ceea5882104e6bdbc555d8b593970b7d19623937
MrBTTF <zhvladi@gmail.com>
2024-05-31 17:35:11 +0200

    loop tests

--- tests/compiler_test.rs
+++ tests/compiler_test.rs
@@ -30,5 +30,5 @@ fn test_hello() {
 fn test_loop() {
     let src = "loop";
     let output = compile_src(&src);
-    assert_eq!(&output, "")
+    assert_eq!(&output, "repeat\r\nrepeat\r\nrepeat\r\nrepeat\r\nrepeat\r\ntest\r\ntest\r\ntest\r\ntest\r\ntest\r\n")
 }
